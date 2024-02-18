28ad1fead2201457baa16431f69adef0aa5016cd
Daniel Kapper <dkapper01@gmail.com>
2024-02-18 15:01:54 -0500

    fetch json and add cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
 const port = 3000;
 
+app.use(cors()); // Use CORS middleware to allow requests from any origin
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!!" });
 });
 
 app.listen(port, () => {
